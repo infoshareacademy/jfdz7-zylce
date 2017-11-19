08de845cadd08bbbdc0e8395a7875e6c7039a432
moni <monika.rusajczyk@gmail.com>
2017-11-19 02:56:01 +0100

    fixed margin for devices >= lg in section team

--- css/js/custom.js
+++ css/js/custom.js
@@ -2,3 +2,13 @@
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+//Hide Newsletter After Click
+$('.btn-newsletter').on('click', function(){
+    if($(".form-check-input").is(':checked'))
+        $(".newsletter-bg").hide();  // checked
+    else
+        $(".newsletter-bg").show();  // unchecked
+
+    // $('.newsletter-bg').hide();
+});
