355142c2dde8b558323f4edc6216e1fc29d0e208
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-01-22 21:18:36 +0100

    fix time of animations, small cleaning in code and change mini logo color.

--- css/js/script.js
+++ css/js/script.js
@@ -70,55 +70,39 @@ $(document).ready(function() {
     const $addIconsLeftPanel = $('.add-icons-left-panel');
     const $addIconsRightPanel = $('.add-icons-right-panel');
     const $teamMember = $('.team-member');
-    const $socialIcons = $('.team-member-social-icons');
 
-    $logo.show(1000, function () {
-        $welcomeText.animate({opacity: 1}, 1500, function () {
-            $joinUsBtn.animate({opacity: 1}, 300);
+    $logo.show(500, function () {
+        $welcomeText.animate({opacity: 1}, 150, function () {
+            $joinUsBtn.animate({opacity: 1}, 100);
         });
     });
-    // $welcomeText.animate({opacity: 1}, 3500);
-    // $joinUsBtn.animate({opacity: 1}, 3500);
 
     function mainIconsVisibility() {
-        if ($(this).scrollTop() > 370) {
-            $mainIcon1.fadeIn(400, function () {
-                $mainIcon2.fadeIn(400, function () {
-                    $mainIcon3.fadeIn(400, function () {
-                        $mainIcon4.fadeIn(400);
+        if ($(this).scrollTop() > 300) {
+            $mainIcon1.fadeIn(500, function () {
+                $mainIcon2.fadeIn(150, function () {
+                    $mainIcon3.fadeIn(150, function () {
+                        $mainIcon4.fadeIn(150);
                     });
                 });
             });
-            // $mainIcon2.fadeIn(1400);
-            // $mainIcon3.fadeIn(2100);
-            // $mainIcon4.fadeIn(2800);
         }}
 
     function addIconsVisibility() {
-        if($(this).scrollTop() > 800) {
+        if($(this).scrollTop() > 700) {
             $addIconsLeftPanel.animate({left: 0}, 700);
             $addIconsRightPanel.animate({left: 0}, 500);
         }
     }
     function teamMemberAnimation() {
-        if($window.scrollTop() >  1000) {
-            $teamMember.animate({top: 0}, 1260 );
+        if($window.scrollTop() >  1100) {
+            $teamMember.animate({left: 0}, 1500 );
         }
 
     }
 
-    //dorobic  zeby ikony pod memberami sie pozniej pojawialy (po djecich)
-
-
-    function write() {
-        if($window.scrollTop() > 1) {
-            console.log('dupa dupa :)');
-            console.log($window.scrollTop() + 'px');
-        }
-
-    }
     $window.on('scroll', mainIconsVisibility);
     $window.on('scroll', addIconsVisibility);
     $window.on('scroll', teamMemberAnimation);
-    $window.on('scroll', write);
+
 });
