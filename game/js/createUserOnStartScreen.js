7eec5d56e7eb8a182f83bf98316f2084c22a83b8
moni <monika.rusajczyk@gmail.com>
2018-01-30 16:12:12 +0100

    first char at user name toUpperCase

--- game/js/createUserOnStartScreen.js
+++ game/js/createUserOnStartScreen.js
@@ -4,7 +4,7 @@ const $startButton = document.getElementById('startGameBtn');
 
 const players = [];
 
-const getPlayerName = () => $playerNameInput.value;
+const getPlayerName = () => $playerNameInput.value.charAt(0).toUpperCase() + $playerNameInput.value.slice(1);
 const addPlayer = () => ( {name: getPlayerName(), score: 0} );
 
 $startButton.addEventListener('click', function() {
