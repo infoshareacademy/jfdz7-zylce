a3f52100452241f113535f82c3d086d8273509a0
moni <monika.rusajczyk@gmail.com>
2018-01-30 15:00:16 +0100

    style input and button when inputtext is valid

--- game/js/startButtonDisability.js
+++ game/js/startButtonDisability.js
@@ -1,8 +1,15 @@
-const isNameMissing = () => { return $playerNameInput.value.length <3 };
+const isNameMissing = () => {
+    const MIN_REQUIRED_NAME_LENGTH = 2;
+    return $playerNameInput.value.length < MIN_REQUIRED_NAME_LENGTH };
 
 const isPlayerNameValid = () => {
-    // $startButton.setAttribute('disabled', isNameMissing());
     isNameMissing() ? $startButton.setAttribute('disabled', '') : $startButton.removeAttribute('disabled')
 };
 
+const changeInputStyle = () => {
+    isNameMissing() ? $playerNameInput.classList.remove('valid') : $playerNameInput.classList.add('valid')
+};
+
 $playerNameInput.addEventListener('keyup', isPlayerNameValid);
+$playerNameInput.addEventListener('keyup', changeInputStyle);
+
