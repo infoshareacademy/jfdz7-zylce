e5cdf07718b83cd84d53de40d6082000c55d64b2
moni <monika.rusajczyk@gmail.com>
2018-01-30 18:37:25 +0100

    timer animation code clean

---
+++ game/js/animateTimeCounter.js
@@ -0,0 +1,15 @@
+$exampleTimeAnimation = document.getElementById('timeAnimated');
+const INITIAL_TIME_LIMIT = 40;
+
+function timeCounter(initialTime) {
+    let timeLeft = initialTime;
+    setInterval(() => {
+        if (timeLeft >= 0) {
+            $exampleTimeAnimation.textContent = timeLeft;
+            timeLeft--;
+        }
+    }, 1000)
+}
+
+const animateTimer = new timeCounter(INITIAL_TIME_LIMIT);
+
