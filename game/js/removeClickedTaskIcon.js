42f3d27442b942d00e1fabb76302e3605df0b787
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-01-31 23:48:04 +0100

    changes in script.

--- game/js/removeClickedTaskIcon.js
+++ game/js/removeClickedTaskIcon.js
@@ -1,23 +1,28 @@
 const tasksOnMap = document.getElementsByClassName('task-on-map');
-const taskToClick = document.getElementById('example-icon');
+const taskToClick = document.getElementsByClassName('game-task');
+generateTasksOnMap(5);
+setInterval(setTask, 1000);
 
 
-generateTasksOnMap(5)
+let newGameTask = document.getElementsByClassName('game-task').src;
 
 for(let i = 0; i < tasksOnMap.length; i++){
     tasksOnMap[i].addEventListener('click', function() {
-        let id = this.id;
-        let imgSrc = this.src;
+        // let id = this.id;
+        // let imgSrc = this.src;
 
         if(this.src === taskToClick.src) {
             console.log('trafiłeś');
             this.remove();
+            setTask()
+
         } else{
             console.log('nie trafiłes');
         }
     })
 
 }
+
 
 
 
