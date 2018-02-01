b76e551ec08284e326318f5c3fddaf3e023714cc
moni <monika.rusajczyk@gmail.com>
2018-02-01 20:43:59 +0100

    change var names

--- game/js/gameTimeChanger.js
+++ game/js/gameTimeChanger.js
@@ -1,6 +1,6 @@
 function changeTime(time) {
     clearInterval(cancel);
     seconds = parseInt(document.getElementById('game-time').innerText) + time;
-    document.getElementById('game-time').textContent = seconds;
+    gameTime.textContent = seconds;
     cancel = setInterval(decrementSeconds, 1000);
 }
--- game/js/gameTimer.js
+++ game/js/gameTimer.js
@@ -1,9 +1,9 @@
 let seconds = parseInt(document.getElementById('game-time').innerText);
-let el = document.getElementById('game-time');
+let gameTime = document.getElementById('game-time');
 
 function decrementSeconds() {
     seconds -= 1;
-    el.innerText = seconds;
+    gameTime.innerText = seconds;
 }
 
 let cancel = setInterval(decrementSeconds, 1000);
