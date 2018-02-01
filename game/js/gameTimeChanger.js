e3a5ef824d1c695013c157004ef5105991a9ebda
moni <monika.rusajczyk@gmail.com>
2018-02-01 20:56:26 +0100

    timer counts down to 0 (negative values excluded)

--- game/js/gameTimeChanger.js
+++ game/js/gameTimeChanger.js
@@ -1,6 +1,12 @@
 function changeTime(time) {
     clearInterval(cancel);
     seconds = parseInt(document.getElementById('game-time').innerText) + time;
-    gameTime.textContent = seconds;
-    cancel = setInterval(decrementSeconds, 1000);
+    console.log('sek: ' + seconds)
+    if ( seconds > 0 ) {
+        gameTime.textContent = seconds;
+        cancel = setInterval(decrementSeconds, 1000);
+    } else {
+        gameTime.innerText = 0;
+        clearInterval(cancel);
+    }
 }
--- game/js/gameTimer.js
+++ game/js/gameTimer.js
@@ -2,8 +2,10 @@ let seconds = parseInt(document.getElementById('game-time').innerText);
 let gameTime = document.getElementById('game-time');
 
 function decrementSeconds() {
-    seconds -= 1;
-    gameTime.innerText = seconds;
+    if (seconds > 0) {
+        seconds -= 1;
+        gameTime.innerText = seconds;
+    }
 }
 
 let cancel = setInterval(decrementSeconds, 1000);
