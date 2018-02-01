6ad3bc08e3b30335950645b2c932065e59a4fb5d
moni <monika.rusajczyk@gmail.com>
2018-02-01 20:59:20 +0100

    timer finished

--- game/js/gameTimeChanger.js
+++ game/js/gameTimeChanger.js
@@ -1,7 +1,6 @@
-function changeTime(time) {
+function changeTime(timeToAdd) {
     clearInterval(cancel);
-    seconds = parseInt(document.getElementById('game-time').innerText) + time;
-    console.log('sek: ' + seconds)
+    seconds = parseInt(document.getElementById('game-time').innerText) + timeToAdd;
     if ( seconds > 0 ) {
         gameTime.textContent = seconds;
         cancel = setInterval(decrementSeconds, 1000);
