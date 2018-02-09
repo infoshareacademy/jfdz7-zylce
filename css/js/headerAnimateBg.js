d065a95a12ded673e93c6f872e32f78d17f3ab92
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-02-09 12:48:24 +0100

    change header bg after site refresh

--- css/js/headerAnimateBg.js
+++ css/js/headerAnimateBg.js
@@ -1,18 +1,8 @@
 $(document).ready(function(){
-
+    var $window = $(window);
     var $animated = $('#animated');
     var $images = ['url("images/header-background-01.jpg")', 'url("images/header-background-02.jpg")', 'url("images/header-background-03.jpg")']
-
-    $animated.css('backgroundImage', 'url("images/header-background-01.jpg")');
-
-    var count = 1;
-    setInterval(function () {
-        $animated.css('backgroundImage', $images[count]);
-        count++;
-        console.log('Obrazek: ' + count);
-        if (count === $images.length ){
-            count = 0
-        }
-    }, 3000)
+    var $randomNum = Math.floor(Math.random()* 3) ;
+    $animated.css('backgroundImage', $images[$randomNum] );
 
 });
---
+++ game/js/afterClickAnimation.js
@@ -0,0 +1,8 @@
+const star = document.createElement('i');
+star.classList.add('fas');
+star.classList.add('fa-star');
+star.classList.add('animaion');
+
+function createStar() {
+    gameMap.appendChild(star);
+}
